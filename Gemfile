ec49928cf00b2499b9473e46abc1ecbae4f4bf3e
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-02-26 00:49:28 +1300

    chore: add graphql

--- Gemfile
+++ Gemfile
@@ -4,36 +4,26 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.2.1'
-
-gem 'rails', '~> 7.0.4', '>= 7.0.4.2'
-
-# The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
-gem 'sprockets-rails'
-
-# Use postgres as the database for Active Record
+gem 'bootsnap', require: false
+gem 'graphql', '~> 2.0'
 gem 'pg'
-
-# Use the Puma web server [https://github.com/puma/puma]
 gem 'puma'
-
-# Use Redis adapter to run Action Cable in production
+gem 'rails', '~> 7.0.4', '>= 7.0.4.2'
 gem 'redis', '~> 4.0'
-
-# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+gem 'sprockets-rails'
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
-# Reduces boot times through caching; required in config/boot.rb
-gem 'bootsnap', require: false
-
 group :development, :test do
-  # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
   gem 'rspec-rails'
 end
 
 group :development do
+  gem 'graphiql-rails'
   gem 'rubocop', require: false
+  gem 'rubocop-graphql', require: false
   gem 'rubocop-rails', require: false
+  gem 'rubocop-rspec', require: false
   gem 'spring'
   gem 'web-console'
 end
---
+++ app/controllers/graphql_controller.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+class GraphqlController < ApplicationController
+  # If accessing from outside this domain, nullify the session
+  # This allows for outside API access while preventing CSRF attacks,
+  # but you'll have to authenticate your user separately
+  # protect_from_forgery with: :null_session
+
+  def execute
+    variables = prepare_variables(params[:variables]) || {}
+    operation_name = params[:operationName]
+    context = {
+      # Query context goes here, for example:
+      # current_user: current_user,
+    }
+    render json: MyTeamChurchApiSchema.execute(params[:query], variables:, context:, operation_name:)
+  rescue StandardError => e
+    raise e unless Rails.env.development?
+
+    handle_error_in_development(e)
+  end
+
+  private
+
+  # Handle variables in form data, JSON body, or a blank value
+  def prepare_variables(variables_param)
+    case variables_param
+    when String
+      return variables_param.present? ? JSON.parse(variables_param) || {} : {}
+    when Hash
+      return variables_param
+    when ActionController::Parameters
+      return variables_param.to_unsafe_hash # GraphQL-Ruby will validate name and type of incoming variables.
+    end
+    raise ArgumentError, "Unexpected parameter: #{variables_param}"
+  end
+
+  def handle_error_in_development(error)
+    logger.error error.message
+    logger.error error.backtrace.join("\n")
+
+    render json: { errors: [{ message: error.message, backtrace: error.backtrace }], data: {} },
+           status: :internal_server_error
+  end
+end
