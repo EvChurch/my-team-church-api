b444a249b4eed8a080a3a2c426bb3b3396bb92a7
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-05-06 21:41:10 +1000

    add auth to sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+if Rails.env.production?
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(Digest::SHA256.hexdigest(username),
+                                                Digest::SHA256.hexdigest(ENV.fetch('SIDEKIQ_USERNAME', nil))) &
+      ActiveSupport::SecurityUtils.secure_compare(Digest::SHA256.hexdigest(password),
+                                                  Digest::SHA256.hexdigest(ENV.fetch('SIDEKIQ_PASSWORD', nil)))
+  end
+end
+
 Rails.application.routes.draw do
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
   post '/graphql', to: 'graphql#execute'
