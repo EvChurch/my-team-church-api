bae571df761edc98380cacc21409eb53b79e72e2
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-05-06 21:56:51 +1000

    add cron web

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'sidekiq/web'
+require 'sidekiq/cron/web'
+
 if Rails.env.production?
   Sidekiq::Web.use Rack::Auth::Basic do |username, password|
     ActiveSupport::SecurityUtils.secure_compare(Digest::SHA256.hexdigest(username),
