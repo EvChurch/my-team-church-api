189af2ce645c610506d3c06380861564a9c85646
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-05-07 00:42:50 +1000

    add objective when returning result

--- app/graphql/mutations/objective/result/delete_mutation.rb
+++ app/graphql/mutations/objective/result/delete_mutation.rb
@@ -9,11 +9,13 @@ module Mutations
 
         argument :id, ID, 'result id to delete', required: true
         field :id, ID, 'id of result after delete', null: true
+        field :objective, ID, 'objective of result after delete', null: true
 
         def resolve(id:)
           result = ::Objective::Result.find(id)
+          objective = result.objective
           result.destroy!
-          { id: }
+          { id:, objective: }
         end
 
         def authorized?(id:)
